1a573415a35dc27bd9adfc90f3138175357470db
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-06-22 12:34:23 -0500

    [update] hello world page

--- website/pages/en/hello-world.js
+++ website/pages/en/hello-world.js
@@ -3,14 +3,16 @@
 const React = require('react')
 const CompLibrary = require('../../core/CompLibrary')
 const Container = CompLibrary.Container
-const GridBlock = CompLibrary.GridBlock
+// const GridBlock = CompLibrary.GridBlock
 
 function HelloWorld (props) {
   return (
-    <Container>
-      <h1>Hello World!</h1>
-      <p>I can write jsx here!</p>
-    </Container>
+    <div className="docMainWrapper wrapper">
+      <Container className="mainContainer documentContainer postContainer">
+        <h1>Hello World!</h1>
+        <p>This is my first page!</p>
+      </Container>
+    </div>
   )
 }
 
